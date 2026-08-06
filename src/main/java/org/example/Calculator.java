313df6b759c8554603ee37434dc751b23f986baf
ekignatenko <egorignatenko53@gmail.com>
2026-08-06 18:47:38 +0300

    Добавил тесты divide, assertThrows, @BeforeEach

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,4 +9,10 @@ public class Calculator {
     public boolean isPositive(int number) {
         return number > 0;
     }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,52 @@
 import org.example.Calculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    Calculator calc;
+
+    @BeforeEach
+    public void setUp() {
+        calc = new Calculator();   // выполнится ПЕРЕД каждым тестом
+    }
+
+    @Test
+    public void testSumm() {
+        int result = calc.sum(2, 3);   // calc уже создан, new не нужен!
+        assertEquals(5, result);
+    }
+
     @Test
     public void testSum() {
-        Calculator calc = new Calculator();
+
         int result = calc.sum(2, 3);
         assertEquals(5, result);
     }
         @Test
         public void testIsPositive() {
-            Calculator calc = new Calculator();
+
             assertTrue(calc.isPositive(5));    // 5 положительное → ожидаем true
             assertFalse(calc.isPositive(-3));  // -3 отрицательное → ожидаем false
         }
+        @Test
+    public void testmultiply() {
+
+            int result = calc.multiply(2, 3);
+            assertEquals(6, result);
+    }
+    @Test
+    public void testDivide() {
+
+        int result = calc.divide(6, 2);
+        assertEquals(3, result);
+    }
+    @Test
+    public void testDivideByZero() {
+
+        assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+    }
     }
 
 
