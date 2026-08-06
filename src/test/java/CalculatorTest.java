d2937d1acc9cacc1533a13c3ad0de18f8a9f22d1
ekignatenko <egorignatenko53@gmail.com>
2026-08-06 19:05:59 +0300

    ParameterizedTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,9 @@
 import org.example.Calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
@@ -46,6 +49,31 @@ public class CalculatorTest {
     public void testDivideByZero() {
 
         assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {5, 100, 7, 999, 1,34,23})
+    public void testIsPositiveMany(int number) {
+        assertTrue(calc.isPositive(number));
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "2, 3, 5",
+            "10, 20, 30",
+            "0, 0, 0",
+            "100, 1, 101"
+    })
+    public void testSumMany(int a, int b, int expected) {
+        assertEquals(expected, calc.sum(a, b));
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "10,2,5",
+            "20,4,5",
+            "9,3,3",
+            "18,3,6"
+    })
+    public void testDevideMany(int a, int b, int expected) {
+        assertEquals(expected, calc.divide(a, b));
     }
     }
 
